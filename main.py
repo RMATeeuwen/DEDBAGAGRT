da6a1f1c32d5c270bf3f0ceadcac7b18b6de2a85
DAANBOER <39552979+DAANBOER@users.noreply.github.com>
2018-05-24 18:08:22 +0200

    updated main.py
    Added .csv files to the git so everyone can test the application

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+import os.path
 import TrafficNodes as tNodes
 import WeatherNodes as wNodes
 
 if __name__ == '__main__':
-    # trafficNodes = tNodes.loadTrafficNodes(
-    #   "C:\\Users\\Daan\\OneDrive\\Master\\Data Engineering\\Data sets\\Final\\Blokkades2017_situatieberichten.csv")
+    trafficNodes = tNodes.loadTrafficNodes(
+        os.path.dirname(os.path.abspath(__file__)) + '\\Blokkades2017_situatieberichten.csv')
 
-    # tNodes.saveTrafficNodes(trafficNodes)
+    weatherNodes = wNodes.loadWeatherNodes(os.path.dirname(os.path.abspath(__file__)) + '\\KNMI_hourly_stripped.txt')
 
-    weatherNodes = wNodes.loadWeatherNodes(
-        "C:\\Users\\Daan\\OneDrive\\Master\\Data Engineering\\Data sets\\Final\\KNMI_hourly_stripped.txt")
-
+    tNodes.saveTrafficNodes(trafficNodes)
     wNodes.saveWeatherNodes(weatherNodes)
 
     print("Done")
